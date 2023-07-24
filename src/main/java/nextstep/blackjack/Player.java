0e7b54eea8d4530e20caaeb1738781203cc2afd3
salbyul <kpast96@gmail.com>
2023-07-24 15:32:44 +0900

    refactor(blackjack): 필드명 변경 amount -> betAmount

--- src/main/java/nextstep/blackjack/Player.java
+++ src/main/java/nextstep/blackjack/Player.java
@@ -5,18 +5,18 @@ import java.util.Objects;
 public class Player {
 
     private final String name;
-    private int amount;
+    private int betAmount;
 
     public Player(final String name) {
         this.name = name;
     }
 
     public void bet(final int amount) {
-        this.amount = amount;
+        this.betAmount = amount;
     }
 
     public int getBetAmount() {
-        return this.amount;
+        return this.betAmount;
     }
 
     public String getName() {
@@ -28,11 +28,11 @@ public class Player {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         final Player player = (Player) o;
-        return amount == player.amount && Objects.equals(name, player.name);
+        return betAmount == player.betAmount && Objects.equals(name, player.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, amount);
+        return Objects.hash(name, betAmount);
     }
 }
