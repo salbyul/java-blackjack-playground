0b3d00c0ebb4c71d99a03e4e7cc88fa90bf58f4a
salbyul <kpast96@gmail.com>
2023-07-25 19:25:43 +0900

    style(blackjack): PlayerList 인라인화

--- src/main/java/nextstep/blackjack/Application.java
+++ src/main/java/nextstep/blackjack/Application.java
@@ -1,20 +1,16 @@
 package nextstep.blackjack;
 
 import nextstep.blackjack.participant.Dealer;
-import nextstep.blackjack.participant.Player;
 import nextstep.blackjack.participant.Players;
 import nextstep.blackjack.view.InputView;
 import nextstep.blackjack.view.ResultView;
-
-import java.util.List;
 
 public class Application {
 
     private static final InputView inputView = new InputView();
 
     public static void run() {
-        List<Player> playerList = inputView.getPlayer();
-        Players players = new Players(playerList);
+        Players players = new Players(inputView.getPlayer());
         Deck.giveTwoCards(players);
         ResultView.printCardsAfterReceiveTwoCards(players);
         inputView.getUserInputGetOneMoreCard(players);
